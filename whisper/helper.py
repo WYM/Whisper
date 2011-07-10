89fb65266edf1df1d251ece08e7d7c6b68b5dfa2
root <root@Cream.(none)>
2011-07-10 12:38:05 +0800

    first version.

--- whisper/helper.py
+++ whisper/helper.py
@@ -1,6 +1,6 @@
 #coding: utf8
 from flask import Module, request, session, g, redirect, url_for, abort, render_template, flash
-import hashlib, urllib
+import hashlib, urllib, datetime
 from whisper import db
 
 helper = Module(__name__)
@@ -17,6 +17,25 @@ def valid_login(username, password, users):
 
 def get_avatar(email, size = 42, default = ''):
 	if not default:
-		default = url_for('static', filename = 'default.jpg')
-	gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+		default = url_for('.static', filename = 'default.jpg')
+	if email:
+		gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+	else:
+		gravatar_url = default;
 	gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+	return gravatar_url
+	
+def time_span(ts):
+   delta = datetime.datetime.utcnow() - ts
+   if delta.days >= 365:
+       return u'%d年前' % (delta.days / 365)
+   elif delta.days >= 30:
+       return u'%d个月前' % (delta.days / 30)
+   elif delta.days > 0:
+       return u'%d天前' % delta.days
+   elif delta.seconds < 60:
+       return u"%d秒前" % delta.seconds
+   elif delta.seconds < 60 * 60:
+       return u"%d分钟前" % (delta.seconds / 60)
+   else:
+       return u"%d小时前" % (delta.seconds / 60 / 60)
--- whisper/views/default.py
+++ whisper/views/default.py
@@ -1,14 +1,42 @@
 #coding: utf8
-from flask import Module, request, session, g, redirect, url_for, abort, render_template, flash, current_app
+from flask import Module, request, session, g, redirect, url_for, abort, render_template, flash, current_app, get_flashed_messages
 from whisper import db
 from whisper.helper import *
+import datetime
 
 default = Module(__name__)
 
 @default.route('/')
 def index():
-	db.test.insert({'name':'Mike', 'age': 17})
-	return str(db.test.find_one())
+	data = db.posts.find()
+	if get_flashed_messages():
+		message = get_flashed_messages()[0]
+	else:
+		message = 'Making splendid shots of our life!'
+	return render_template('index.html', data = data, message = message, get_avatar = get_avatar, time_span = time_span)
+
+@default.route('/post/', methods=['POST', 'GET'])
+def post():
+	if request.method == 'POST' and request.form['name'] and request.form['email'] and request.form['content']:
+		db.posts.insert({
+			'id': db.ids.find_and_modify({"name":"posts"}, {'$inc':{'id':1}}, True),
+			'name': request.form['name'],
+			'email': request.form['email'],
+			'content': request.form['content'],
+			'dateline': datetime.datetime.utcnow(),
+			'comments': [],
+		})
+		
+		flash(request.form['name'] + u'，您的帖子已经发表成功~')
+	else:
+		flash(request.form['name'] + u'，请填写所有需要的项目后再发表~')
+	return(redirect(url_for('index')))
+
+@default.route('/star/<int:pid>/')
+def star(pid):
+	#db.posts.update({'id': pid}, {'$set': {'star' : 1}}, True)
+	flash(u'成功给帖子加上星标')
+	return(redirect(url_for('index')))
 
 @default.route('/login/', methods=['GET', 'POST'])
 def login():
